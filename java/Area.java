c1a432d80ad027d8fd868afd8f82d7e7ca02135f
tbfWOI <trevorjoins@gmail.com>
2016-01-21 09:11:05 +0530

    Adding Area program

---
+++ java/Area.java
@@ -0,0 +1,10 @@
+class Area{
+    public static void main(String args[]){
+        System.out.println("This is a program to calculate Area.");
+        double r, pi, area;
+        r = 10.2; 
+        pi = 3.1416;
+        area = pi * r * r;
+        System.out.println("The area of the circle is: " + area);
+    }
+}
