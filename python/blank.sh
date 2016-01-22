820368d876b894d0021f7a95c5393ecb50cb3625
tbfWOI <trevorjoins@gmail.com>
2016-01-22 20:31:43 +0530

    Adding files from office

---
+++ python/blank.sh
@@ -0,0 +1,11 @@
+            id: settingsId1
+            property variant settings_object : language_object.language_value.settings
+            property variant call_attendant : settingsId1.settings_object.call_attendant.value
+            property variant call_attendant_title : settingsId1.settings_object.call_attendant_title.value
+            property variant cancel_call : settingsId1.settings_object.cancel_call.value
+            property variant common_close : settingsId1.settings_object.common_close.value
+            property variant popup_attendant : settingsId1.settings_object.popup_attendant.value
+            property variant popup_brightness : settingsId1.settings_object.popup_brightness.value
+            property variant popup_mute : settingsId1.settings_object.popup_mute.value
+            property variant popup_reading : settingsId1.settings_object.popup_reading.value
+            property variant popup_volume : settingsId1.settings_object.popup_volume.value
---
+++ python/first.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+def a_bigger(a,b):
+  if a > b:
+    print "a is greater than b"
+  if a < b:
+    print "b is greater than a"
+
+a_bigger(10,20)
+a_bigger(410,20)
